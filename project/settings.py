313f05e9343062089634830ae63293581e01fa9c
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-05-19 19:35:09 +0300

    Выполнение задания №3 django ORM
    Удалил main.py, добавил manage.py
    Добавил переменные окружения в settings.py
    Добавил readme
    Добавил в requirements environs

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,37 @@
 import os
+#from dotenv import load_dotenv
+#load_dotenv()
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('ENGINE'),
+#         'HOST': os.getenv('HOST'),
+#         'PORT': os.getenv('PORT'),
+#         'NAME': os.getenv('NAME'),
+#         'USER': os.getenv('USER'),
+#         'PASSWORD': os.getenv('PASSWORD'),
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': env('ENGINE'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
     }
 }
-
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ROOT_URLCONF = 'project.urls'
 
